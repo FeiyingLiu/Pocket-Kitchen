8924e6053a4983749e91d5320ae358a2981f1afc
刘翡莹 <Feiying@dhcp-10-105-120-184.wireless.northwestern.private>
2016-09-22 22:21:30 -0500

    First commit

---
+++ src/api/RpcParser.java
@@ -0,0 +1,60 @@
+package api;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+/**
+ * A utility class to handle rpc related parsing logics. turn request to JSONObeject
+ */
+public class RpcParser {
+	public static JSONObject parseInput(HttpServletRequest request) {
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null) {
+				jb.append(line);
+			}
+			reader.close();
+			return new JSONObject(jb.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	// two write ways, create those two method for users to write in date
+	public static void writeOutput(HttpServletResponse response, JSONObject obj) {
+		try {			
+			response.setContentType("application/json");
+			// control authority
+			response.addHeader("Access-Control-Allow-Origin", "*");
+			PrintWriter out = response.getWriter();
+			out.print(obj);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public static void writeOutput(HttpServletResponse response, JSONArray array) {
+		try {			
+			response.setContentType("application/json");
+			response.addHeader("Access-Control-Allow-Origin", "*");
+			PrintWriter out = response.getWriter();
+			out.print(array);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+}
---
+++ src/db/MongoDBImport.java
@@ -0,0 +1,24 @@
+package db;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+import java.text.ParseException;
+
+public class MongoDBImport {
+    public static void main(String[] args) throws ParseException {
+     // main allows to run alone 
+   	 MongoClient mongoClient = new MongoClient();
+   	 MongoDatabase db = mongoClient.getDatabase(DBUtil.DB_NAME);
+   	 // if user didn't exist, will create a new one automatically
+   	 db.getCollection("users").insertOne( 
+   			 new Document()
+   					 .append("first_name", "John")
+   					 .append("last_name", "Smith")
+   					 .append("password", "3229c1097c00d497a0fd282d586be050")
+   					 .append("user_id", "1111"));
+   	 mongoClient.close();
+    }
+}
